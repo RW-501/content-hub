8ee5b9b2c4fb3c949d2856f0105780a02ad03a47
Ronald Wilson II <92776143+RW-501@users.noreply.github.com>
2025-09-22 12:08:02 -0500

    up

--- site/js/footer.js
+++ site/js/footer.js
@@ -36,6 +36,8 @@ export function loadFooter(targetId = "main-footer") {
         <span>|</span>
         <a href="https://contenthub.guru/category" style="color:#f3f4f6; text-decoration:none;">Categories</a>
         <span>|</span>
+        <a href="https://contenthub.guru/site/lang/" style="color:#f3f4f6; text-decoration:none;">languages</a>
+        <span>|</span>
         <a href="https://contenthub.guru/site/feedback" style="color:#f3f4f6; text-decoration:none;">Feedback</a>
         <span>|</span>        
         <a href="https://contenthub.guru/site/faqs" style="color:#f3f4f6; text-decoration:none;">FAQs</a>
